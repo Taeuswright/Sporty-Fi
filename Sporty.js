02733577dea172ff8adc9213b900dec92e424172
Taeus Wright <taeus@pecsafety.com>
2019-06-27 08:52:19 -0500

    finished

--- Sporty.js
+++ Sporty.js
@@ -2,409 +2,67 @@ var json = {
   items: [
     {
       name: "Work",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id: "OSxkYzM0YjgwOTFhZGJhZjQxOTlkMTY2MzgwZTBiOTE1ODRjMjFlMzIw",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/3Ygf6yJwbrsXXcx9h0pOv4/tracks",
-        total: 2
-      },
-      type: "playlist",
-      uri: "spotify:playlist:3Ygf6yJwbrsXXcx9h0pOv4"
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/6m6ZmiBlSN8A9DVprsJkrt"
-      },
-      href: "https://api.spotify.com/v1/playlists/6m6ZmiBlSN8A9DVprsJkrt",
-      id: "6m6ZmiBlSN8A9DVprsJkrt",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/0a16c89b313ada7f8d935ebddebdf610377d4ae5324df75e87a75f4188ea6f907de9774c4962cede3b673c999773ba34acd5d724d47666e407fa06f253c15e25a96272ee3690dcf5f799d42a8db89f84",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/0a16c89b313ada7f8d935ebddebdf610377d4ae5324df75e87a75f4188ea6f907de9774c4962cede3b673c999773ba34acd5d724d47666e407fa06f253c15e25a96272ee3690dcf5f799d42a8db89f84",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/0a16c89b313ada7f8d935ebddebdf610377d4ae5324df75e87a75f4188ea6f907de9774c4962cede3b673c999773ba34acd5d724d47666e407fa06f253c15e25a96272ee3690dcf5f799d42a8db89f84",
-          width: 60
-        }
-      ],
       name: "Bailey Songs",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id: "OCwyOGZiYTYyMjI0MDE4NDBiMThkODFmMDRkYzZiMjcwYTIyNWQ4ZThh",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/6m6ZmiBlSN8A9DVprsJkrt/tracks",
-        total: 7
-      },
-      type: "playlist",
-      uri: "spotify:playlist:6m6ZmiBlSN8A9DVprsJkrt"
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/0isvt8dDJdzy1qmCqspwFv"
-      },
-      href: "https://api.spotify.com/v1/playlists/0isvt8dDJdzy1qmCqspwFv",
-      id: "0isvt8dDJdzy1qmCqspwFv",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/5ffa732ea25ea0754425a2e84ae713e67ef46c916e0bf8b8b34b28ddb2f8692c63ec633ae062c4f7f775d237f1aeccc07b10c8cca7607a535aa889fdf915c2e8c0d4087cb25a35602d644c5633ee2e9b",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/5ffa732ea25ea0754425a2e84ae713e67ef46c916e0bf8b8b34b28ddb2f8692c63ec633ae062c4f7f775d237f1aeccc07b10c8cca7607a535aa889fdf915c2e8c0d4087cb25a35602d644c5633ee2e9b",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/5ffa732ea25ea0754425a2e84ae713e67ef46c916e0bf8b8b34b28ddb2f8692c63ec633ae062c4f7f775d237f1aeccc07b10c8cca7607a535aa889fdf915c2e8c0d4087cb25a35602d644c5633ee2e9b",
-          width: 60
-        }
-      ],
       name: "Video Games ",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id:
-        "ODEsMWU1OGQxZGY0ZDcxY2JjNzk1NzQ2YjM5ZDQ0ZjAyYTM0YjM0Yjc2Zg==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/0isvt8dDJdzy1qmCqspwFv/tracks",
-        total: 76
-      },
-      type: "playlist",
-      uri: "spotify:playlist:0isvt8dDJdzy1qmCqspwFv"
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/4WypHQLmVQJNUL1D7WcQCj"
-      },
-      href: "https://api.spotify.com/v1/playlists/4WypHQLmVQJNUL1D7WcQCj",
-      id: "4WypHQLmVQJNUL1D7WcQCj",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/0161fc9b18e8534f479aee269269be8da54adb15248678685fe6a2467dc47a9d49e40dafe9427e93b59a48c5ba4840acad6532b3c6a74934dd5e4c27c24496ada3e30d2cda48b5bee83654ddc9c28e59",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/0161fc9b18e8534f479aee269269be8da54adb15248678685fe6a2467dc47a9d49e40dafe9427e93b59a48c5ba4840acad6532b3c6a74934dd5e4c27c24496ada3e30d2cda48b5bee83654ddc9c28e59",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/0161fc9b18e8534f479aee269269be8da54adb15248678685fe6a2467dc47a9d49e40dafe9427e93b59a48c5ba4840acad6532b3c6a74934dd5e4c27c24496ada3e30d2cda48b5bee83654ddc9c28e59",
-          width: 60
-        }
-      ],
       name: "Study",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id:
-        "NjEsOTViNDMxZDUxMDI2MTMwNDQ0NjJjNGZhZGZlYzhkYzUxYzQzZWFkNQ==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/4WypHQLmVQJNUL1D7WcQCj/tracks",
-        total: 76
-      },
-      type: "playlist",
-      uri: "spotify:playlist:4WypHQLmVQJNUL1D7WcQCj"
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/023hqZqgVV5fqqUfGoEWGW"
-      },
-      href: "https://api.spotify.com/v1/playlists/023hqZqgVV5fqqUfGoEWGW",
-      id: "023hqZqgVV5fqqUfGoEWGW",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/0a16c89b313ada7f8d935ebddebdf610377d4ae52f23e5afa2d7ec6ce93f939d279891cae60582fea71b46bad155e2d98c5e19e47589b7cd8353859fd3f9c8083fc8fd7892ca0e8260e8bc660b15c205",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/0a16c89b313ada7f8d935ebddebdf610377d4ae52f23e5afa2d7ec6ce93f939d279891cae60582fea71b46bad155e2d98c5e19e47589b7cd8353859fd3f9c8083fc8fd7892ca0e8260e8bc660b15c205",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/0a16c89b313ada7f8d935ebddebdf610377d4ae52f23e5afa2d7ec6ce93f939d279891cae60582fea71b46bad155e2d98c5e19e47589b7cd8353859fd3f9c8083fc8fd7892ca0e8260e8bc660b15c205",
-          width: 60
-        }
-      ],
       name: "Food for the heart",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id:
-        "MzIsNTg2NjZhODJkZjE2YWJiM2VjMWM4ODczZmYxMWUwNzQwMDJmZjVhOQ==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/023hqZqgVV5fqqUfGoEWGW/tracks",
-        total: 31
-      },
-      type: "playlist",
-      uri: "spotify:playlist:023hqZqgVV5fqqUfGoEWGW"
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/6xxrQqQy8YeJWVutoyQhzl"
-      },
-      href: "https://api.spotify.com/v1/playlists/6xxrQqQy8YeJWVutoyQhzl",
-      id: "6xxrQqQy8YeJWVutoyQhzl",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/8b75671c743b3200aa273cc25790660f7af9d320cdb8bbdbab86e52bbd4c08e5cd186343ce9fbaebfade577145599daff924bb7b28386a84f67bd1dbff83dfb816c59dc9f70fdb6a83849073ab0009da",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/8b75671c743b3200aa273cc25790660f7af9d320cdb8bbdbab86e52bbd4c08e5cd186343ce9fbaebfade577145599daff924bb7b28386a84f67bd1dbff83dfb816c59dc9f70fdb6a83849073ab0009da",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/8b75671c743b3200aa273cc25790660f7af9d320cdb8bbdbab86e52bbd4c08e5cd186343ce9fbaebfade577145599daff924bb7b28386a84f67bd1dbff83dfb816c59dc9f70fdb6a83849073ab0009da",
-          width: 60
-        }
-      ],
       name: "Hip hop",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id:
-        "NDAsNGE0YTIwYWJhMmY2M2UyNzJjOGU3YThmZTVmYTMxMjA5NTYzOWY4Ng==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/6xxrQqQy8YeJWVutoyQhzl/tracks",
-        total: 35
-      },
-      type: "playlist",
-      uri: "spotify:playlist:6xxrQqQy8YeJWVutoyQhzl"
+
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/4JrlE3amE5RsXznVe837u6"
-      },
-      href: "https://api.spotify.com/v1/playlists/4JrlE3amE5RsXznVe837u6",
-      id: "4JrlE3amE5RsXznVe837u6",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/22da875c060f901998e1f3518ab8fc08d814f92653c15e25a96272ee3690dcf5f799d42a8db89f848446efaa818fa4675af99f48ea978f9de782cd95ad4b7a621db877f249bf1f24dc19cb00d9147b5c",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/22da875c060f901998e1f3518ab8fc08d814f92653c15e25a96272ee3690dcf5f799d42a8db89f848446efaa818fa4675af99f48ea978f9de782cd95ad4b7a621db877f249bf1f24dc19cb00d9147b5c",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/22da875c060f901998e1f3518ab8fc08d814f92653c15e25a96272ee3690dcf5f799d42a8db89f848446efaa818fa4675af99f48ea978f9de782cd95ad4b7a621db877f249bf1f24dc19cb00d9147b5c",
-          width: 60
-        }
-      ],
       name: "Emotions",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id:
-        "OTMsOTM3MmFkMzFlZTJiMzZjYzg5ZmVlMTg5Y2YwODNiMmUwNjRkODg4MA==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/4JrlE3amE5RsXznVe837u6/tracks",
-        total: 74
-      },
-      type: "playlist",
-      uri: "spotify:playlist:4JrlE3amE5RsXznVe837u6"
+      collaborative: false
     },
     {
-      collaborative: false,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/4dlbMU8a3fleqEO0bxjXGw"
-      },
-      href: "https://api.spotify.com/v1/playlists/4dlbMU8a3fleqEO0bxjXGw",
-      id: "4dlbMU8a3fleqEO0bxjXGw",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/02405ab0c95a032170770f4778721711b8de237322da875c060f901998e1f3518ab8fc08d814f92653c15e25a96272ee3690dcf5f799d42a8db89f84b401cc908c99afbb56335e285fa8d373fe176fcc",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/02405ab0c95a032170770f4778721711b8de237322da875c060f901998e1f3518ab8fc08d814f92653c15e25a96272ee3690dcf5f799d42a8db89f84b401cc908c99afbb56335e285fa8d373fe176fcc",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/02405ab0c95a032170770f4778721711b8de237322da875c060f901998e1f3518ab8fc08d814f92653c15e25a96272ee3690dcf5f799d42a8db89f84b401cc908c99afbb56335e285fa8d373fe176fcc",
-          width: 60
-        }
-      ],
       name: "Montage",
-      owner: {
-        display_name: "TaeJ W.",
-
-        href: "https://api.spotify.com/v1/users/vtp9ybgdxtl52e6g6xe1fs9yq",
-        id: "vtp9ybgdxtl52e6g6xe1fs9yq",
-        type: "user",
-        uri: "spotify:user:vtp9ybgdxtl52e6g6xe1fs9yq"
-      },
+      owner: "TaeJ W.",
       primary_color: null,
       public: true,
-      snapshot_id:
-        "NzcsZGE3YjFjOTA3NmFjN2MzNzZlYmFkOTIxMDFhNTZiNzdmZjBjMDRkZA==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/4dlbMU8a3fleqEO0bxjXGw/tracks",
-        total: 66
-      },
-      type: "playlist",
-      uri: "spotify:playlist:4dlbMU8a3fleqEO0bxjXGw"
+
+      collaborative: true
     },
     {
-      collaborative: true,
-      external_urls: {
-        spotify: "https://open.spotify.com/playlist/1r2T4FafEl26kCRw788Pas"
-      },
-      href: "https://api.spotify.com/v1/playlists/1r2T4FafEl26kCRw788Pas",
-      id: "1r2T4FafEl26kCRw788Pas",
-      images: [
-        {
-          height: 640,
-          url:
-            "https://mosaic.scdn.co/640/2e82bac8c64908ef1504e8391362c030b0fdad2eda39a8f80539ea048011ee3aa59f4066659290eaeb3f2e29a74cbe9bfff85599873da3198414b5eeecbc71f031d40de0c941e33a661e3aca1b342255",
-          width: 640
-        },
-        {
-          height: 300,
-          url:
-            "https://mosaic.scdn.co/300/2e82bac8c64908ef1504e8391362c030b0fdad2eda39a8f80539ea048011ee3aa59f4066659290eaeb3f2e29a74cbe9bfff85599873da3198414b5eeecbc71f031d40de0c941e33a661e3aca1b342255",
-          width: 300
-        },
-        {
-          height: 60,
-          url:
-            "https://mosaic.scdn.co/60/2e82bac8c64908ef1504e8391362c030b0fdad2eda39a8f80539ea048011ee3aa59f4066659290eaeb3f2e29a74cbe9bfff85599873da3198414b5eeecbc71f031d40de0c941e33a661e3aca1b342255",
-          width: 60
-        }
-      ],
       name: "xi.xix.xix",
-      owner: {
-        display_name: "kmarti481",
-        external_urls: {
-          spotify: "https://open.spotify.com/user/kmarti481"
-        },
-        href: "https://api.spotify.com/v1/users/kmarti481",
-        id: "kmarti481",
-        type: "user",
-        uri: "spotify:user:kmarti481"
-      },
+      owner: "kMarti481",
       primary_color: null,
-      public: false,
-      snapshot_id:
-        "MjksMDVjNDZiZTJiYjRhZjQyY2I0NjUyOGQxYTAzYzNiYWQ4NzY1ZjMyMQ==",
-      tracks: {
-        href:
-          "https://api.spotify.com/v1/playlists/1r2T4FafEl26kCRw788Pas/tracks",
-        total: 4
-      },
-      type: "playlist",
-      uri: "spotify:playlist:1r2T4FafEl26kCRw788Pas"
+      public: false
     }
   ]
 };
